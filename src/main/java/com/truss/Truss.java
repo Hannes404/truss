8fe05408d3bf4a8336aba74a36870999256f5f80
Hannes <hbenscheck@gmail.com>
2026-04-23 19:24:41 +0200

    Initial commit

---
+++ src/main/java/com/truss/Truss.java
@@ -0,0 +1,15 @@
+package com.truss;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Truss implements ModInitializer {
+	public static final String MOD_ID = "truss";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/truss/TrussDataGenerator.java
@@ -0,0 +1,11 @@
+package com.truss;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TrussDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
