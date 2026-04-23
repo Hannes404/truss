e19ab44da2234035de92d577d7ed81cf8b13ac60
Hannes <hbenscheck@gmail.com>
2026-04-23 19:29:45 +0200

    Complite the setup

---
+++ src/main/java/com/truss/TrussClient.java
@@ -0,0 +1,10 @@
+package com.truss;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TrussClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
