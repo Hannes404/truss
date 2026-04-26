50dc466c58bebaf42739f3b333f167eada0cbcfc
Hannes <hbenscheck@gmail.com>
2026-04-26 15:39:55 +0200

    Models added (without rotation) (newnew)

--- src/main/java/com/truss/block/ModBlocks.java
+++ src/main/java/com/truss/block/ModBlocks.java
@@ -1,13 +1,10 @@
 package com.truss.block;
 
 import com.truss.Truss;
-import com.truss.block.custom.trussblockcustom;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
@@ -15,22 +12,19 @@ import net.minecraft.util.Identifier;
 
 public class ModBlocks {
 
-    public static final Block TRUSS_BLOCK = registerBlock("truss_block",
+    public static final Block TRUSS_BLOCK = registerBlock(
             new Block(AbstractBlock.Settings.create().strength(0.5F)
                     .requiresTool().sounds(BlockSoundGroup.METAL).nonOpaque()));
 
-    private static Block registerBlock(String name, Block block) {
-        registerBlockItem(name, block);
-        return Registry.register(Registries.BLOCK, Identifier.of(Truss.MOD_ID, name), block);
+    private static Block registerBlock(Block block) {
+        registerBlockItem(block);
+        return Registry.register(Registries.BLOCK, Identifier.of(Truss.MOD_ID, "truss_block"), block);
     }
 
-    private static void registerBlockItem(String name, Block block) {
-        Registry.register(Registries.ITEM, Identifier.of(Truss.MOD_ID, name),
+    private static void registerBlockItem(Block block) {
+        Registry.register(Registries.ITEM, Identifier.of(Truss.MOD_ID, "truss_block"),
                 new BlockItem(block, new Item.Settings()));
     }
-
-    //public static final Block TRUSSBLOCKCUSTOM = registerBlock("truss_block_custom",
-    //        new trussblockcustom() //Hier Weitermachen
 
     public static void registerModBlocks() {
         Truss.LOGGER.info("Registering ModBlocks for " + Truss.MOD_ID);
